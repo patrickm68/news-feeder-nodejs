f52862846e92d5a62d837cf576cb3682ce6e274f
patrickm68 <patrick.mcenaney68@gmail.com>
2023-06-13 16:50:30 +0200

    feat: added RSS validation script

---
+++ scripts/validate.js
@@ -0,0 +1,36 @@
+import got from 'got'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import jsdom from 'jsdom'
+
+const { JSDOM } = jsdom
+
+const xmlFile = join(process.cwd(), 'feed.xml')
+const xml = readFileSync(xmlFile, 'utf8')
+
+try {
+  const data = await got.post('https://validator.w3.org/feed/check.cgi', {
+    form: {
+      rawdata: xml,
+      manual: 1
+    }
+  }).text()
+
+  // Avoid importing CSS in the document
+  const dom = new JSDOM(data.replace(/@import.*/gm, ''))
+
+  const title = dom.window.document.querySelector('h2').textContent
+  const recommendations = dom.window.document.querySelector('ul').textContent
+
+  console.log(recommendations)
+
+  if (title === 'Sorry') {
+    console.log('🚨 Feed is invalid!')
+    process.exit(1)
+  } else {
+    console.log('✅ Feed is valid!')
+  }
+} catch (error) {
+  console.log('Service is down')
+  console.error(error.response.statusCode)
+}
